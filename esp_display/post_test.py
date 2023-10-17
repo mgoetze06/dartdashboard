b786b378ffa8ac5408ed70602a5ac4a740df49bd
Maurice Götze <99mg24@web.de>
2023-10-17 21:11:43 +0200

    feat-database: simple webserver for esp reacting to post message

---
+++ esp_display/post_test.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+url = "http://192.168.0.214/update"
+data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
+print(r)
