e5b0bf4f3bcdf63e918635f1f33c56418fb6c16d
Maurice Götze <99mg24@web.de>
2023-10-19 22:37:47 +0200

    zwei spielstände auf 2 displays

--- esp_display/post_test.py
+++ esp_display/post_test.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = "http://192.168.0.214/update"
-data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
+data = {'punkte0': '501', 'punkte1': '501', 'spieler': '0'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 r = requests.post(url, data=json.dumps(data), headers=headers)
 print(r)
